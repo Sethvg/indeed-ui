d561870d967b3e1e6b10113fe4aba7cc79994248
Seth Van Grinsven <me@sethvg. com>
2018-10-10 19:43:36 -0700

    Fixed Rad bug

--- src/game.ts
+++ src/game.ts
@@ -59,7 +59,7 @@ export class Game{
         console.log("Spawning: " + count);
         for(let a = 0; a < count; a++){
             let radius = MathUtil.GetRandomInRange(this.minRadius,this.maxRadius);
-            let xPos = MathUtil.GetRandomInRange(0, this.width-radius);
+            let xPos = MathUtil.GetRandomInRange(0, (this.width-radius*2);
             let dot = new Dot(radius, xPos);
             this.state.push(dot);
         }
